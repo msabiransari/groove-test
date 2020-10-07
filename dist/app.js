7ca8495e79afffd01ffbdb0fb958782e3262479c
Muhammad Sabir <msabiransari@gmail.com>
2020-10-06 22:13:36 -0400

    WIP

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,12 @@
-<script id="script-T8BPuTEcwX">
-  window.onload = function() {
-    class Greetings extends React.Component {
-      render() {
-        return React.createElement('h1', null, 'Groove Greetings, ' + this.props.name + '!');
-      }
+window.onload = function() {
+  class Greetings extends React.Component {
+    render() {
+      return React.createElement('h1', null, 'Groove Greetings, ' + this.props.name + '!');
     }
+  }
 
-    ReactDOM.render(
-      React.createElement(Greetings, { name : 'Muhammad Sabir' }),
-      document.getElementById('app')
-    );
-  };
-</script>
+  ReactDOM.render(
+    React.createElement(Greetings, { name : 'Muhammad Sabir' }),
+    document.getElementById('app')
+  );
+};
