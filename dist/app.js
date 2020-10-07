5055bbc43a170a4cd7cc38ba3691506c13e2679b
Muhammad Sabir <msabiransari@gmail.com>
2020-10-07 00:08:48 -0400

    WIP

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,24 @@
 window.onload = function() {
+  const firebase = require("firebase");
+  require("firebase/firestore");
+
+  firebase.initializeApp({
+    apiKey: 'AIzaSyBzQLxYmWIOATomDgkhmddF2BTa6lFwKb8',
+    authDomain: 'https://groove-test-af9ed.firebaseio.com',
+    projectId: 'groove-test-af9ed'
+  });
+
+  console.log("Firebase initialized ***** ");
+
+  var db = firebase.firestore();
+
+  db.collection("businesses").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+          console.log(`${doc.id} => ${doc.data()}`);
+      });
+  });
+
+
   class Greetings extends React.Component {
     render() {
       return React.createElement('h1', null, 'Groove Greetings, ' + this.props.name + '!');
