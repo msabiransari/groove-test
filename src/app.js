b5771e168c1042762877a84a1e08f2bf533417e3
Muhammad Sabir <msabiransari@gmail.com>
2020-10-10 23:34:20 -0400

    WIP

--- src/app.js
+++ src/app.js
@@ -1,57 +1,53 @@
-window.onload = function() {
-  console.log("React Application Initializing ***** ");
+var appUrl = "https://local-biz.herokuapp.com/";
 
-  var appUrl = "https://local-biz.herokuapp.com/";
+class BusinessList extends React.Component {
+  constructor(props) {
+    super(props);
 
-  class BusinessList extends React.Component {
-    constructor(props) {
-      super(props);
-
-      this.state = {
-        gridOptions: {
-          columnDefs: [
-            {headerName: "Business Name", field: "name"},
-            {headerName: "Street", field: "street"},
-            {headerName: "City", field: "city"},
-            {headerName: "State/Province", field: "state"},
-            {headerName: "Zip/Postal", field: "zip"},
-            {headerName: "Phone", field: "phone"}
-          ],
-          rowData: []
-        }
+    this.state = {
+      gridOptions: {
+        columnDefs: [
+          {headerName: "Business Name", field: "name"},
+          {headerName: "Street", field: "street"},
+          {headerName: "City", field: "city"},
+          {headerName: "State/Province", field: "state"},
+          {headerName: "Zip/Postal", field: "zip"},
+          {headerName: "Phone", field: "phone"}
+        ],
+        rowData: []
       }
-
-      this.loadData();
     }
 
-    loadData() {
-      console.log("Fetching data from " + appUrl);
-      fetch(appUrl)
-        .then(res => res.json())
-        .then(
-          (data) => {
-            console.log("Data loaded ", data);
-            const go = this.state.gridOptions;
-            go.rowData = data;
-            this.setState({
-              gridOptions: go
-            });
-          },
-          (error) => {
-            console.log("Error while loading data", error);
-          }
-        );
-    }
-
-    render() {
-      console.log("State data ", this.state);
-      return (
-        <div className="ag-theme-alpine" style={ { height: 400, width: 600 } }>
-          <AgGridReact gridOptions={this.state.gridOptions}></AgGridReact>
-        </div>
-      );
-    }
+    this.loadData();
   }
 
-  ReactDOM.render(React.createElement(BusinessList), document.getElementById('app'));
-};
+  loadData() {
+    console.log("Fetching data from " + appUrl);
+    fetch(appUrl)
+      .then(res => res.json())
+      .then(
+        (data) => {
+          console.log("Data loaded ", data);
+          const go = this.state.gridOptions;
+          go.rowData = data;
+          this.setState({
+            gridOptions: go
+          });
+        },
+        (error) => {
+          console.log("Error while loading data", error);
+        }
+      );
+  }
+
+  render() {
+    console.log("State data ", this.state);
+    return (
+      <div className="ag-theme-alpine" style={ { height: 400, width: 600 } }>
+        <AgGridReact gridOptions={this.state.gridOptions}></AgGridReact>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(React.createElement(BusinessList), document.getElementById('app'));
